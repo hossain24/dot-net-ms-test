cd2c022f66c579e7bf6244130613ea782032c9c1
hossain24 <hossain24@users.noreply.github.com>
2021-12-21 00:54:18 +0000

    Add files via upload

---
+++ DotNet/DotNet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DotNet {
+    public class BookAvailability {
+        public Book WrittenBook { get; set; }
+
+        public bool AvailabilityOfBook (Book book) {
+
+            if (book.IsAvailable)
+                return true;
+            if (WrittenBook == book)
+                return true;
+
+            return false;
+
+            //return (book.IsAvailable || WrittenBook == book);
+        }
+    }
+
+    public class Book {
+        public bool IsAvailable { get; set; }
+    }
+}
